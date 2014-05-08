d43b77e5d0501075293ab4b9039d5e73b3bf5537
jden <jason@denizac.org>
2014-05-08 14:26:07 -0700

    refactor router; add /stops route

--- http.js
+++ http.js
@@ -1,70 +1,85 @@
 var http = require('http')
+var Router = require('routes-router')
 var sseSerialize = require('event-source-writer').serialize
+
+var fs = require('fs')
+var stops = JSON.parse(fs.readFileSync('./node_modules/data-carta-routes/stops.geojson').toString())
 
 const BOM = '\ufeff'
 
 function server(state) {
 
+  var router = new Router
+
   var serv = http.createServer(function (req, res) {
     
-    if (req.url == '/buses') {
-      getBuses(req, res, state)
-    } else if (req.url == '/buses/tail') {
-      tailBuses(req, res, state)
-    } else {
-      defaultRoute(req, res)
-    }
+    res.setHeader('access-control-allow-origin','*')
+    res.setHeader('access-control-allow-methods', 'GET')
+
+    router(req, res)
 
     console.log(Date.now(), req.method, req.url, req.headers['user-agent'])
   })
 
+
+  router.addRoute('/buses', getBuses(state))
+  router.addRoute('/buses/tail', tailBuses(state))
+  router.addRoute('/stops', getStops)
+
+  router.addRoute('*', defaultRoute)
+
   return serv
 }
 
-function getBuses(req, res, state) {
-  res.setHeader('access-control-allow-origin','*')
-  res.setHeader('access-control-allow-methods', 'GET')
-  res.setHeader('content-type', 'application/json')
-  res.end(JSON.stringify(state.parsed))
+function getBuses(state) {
+  return function (req, res) {
+    res.setHeader('content-type', 'application/json')
+    res.end(JSON.stringify(state.parsed))  
+  }
 }
 
-function tailBuses(req, res, state) {
+function tailBuses(state) {
+  return function (req, res) {
 
-  req.socket.setNoDelay(true)
+    req.socket.setNoDelay(true)
 
-  var contentType = req.headers.accept.indexOf('text/event-stream') >= 0
-    ? 'text/event-stream'
-    : 'text/plain; charset=UTF-8'
+    var contentType = req.headers.accept.indexOf('text/event-stream') >= 0
+      ? 'text/event-stream'
+      : 'text/plain; charset=UTF-8'
 
-  res.writeHead(200, {
-    'content-type': contentType,
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET',
-    'x-content-type-options': 'nosniff'
-  })
+    res.writeHead(200, {
+      'content-type': contentType,
+      'x-content-type-options': 'nosniff'
+    })
 
-  // get browsers to Do The Right Thing(tm)
-  // see https://code.google.com/p/chromium/issues/detail?id=106150
-  res.write(BOM)
+    // get browsers to Do The Right Thing(tm)
+    // see https://code.google.com/p/chromium/issues/detail?id=106150
+    res.write(BOM)
 
-  res.write(':connected\n\n')
+    res.write(':connected\n\n')
 
-  state.parsed.features.forEach(function (feature) {
-    res.write(sseSerialize('add', feature))
-  })
+    state.parsed.features.forEach(function (feature) {
+      res.write(sseSerialize('add', feature))
+    })
 
-  state.changes.pipe(res)
-  console.log(state.changes._readableState.pipesCount + ' tails')
-  res.on('close', function () {
-    state.changes.unpipe(res)
+    state.changes.pipe(res)
     console.log(state.changes._readableState.pipesCount + ' tails')
-  })
+    res.on('close', function () {
+      state.changes.unpipe(res)
+      console.log(state.changes._readableState.pipesCount + ' tails')
+    })
+  }
 }
 
 function defaultRoute(req, res) {
   res.statusCode = 400
   res.setHeader('content-type', 'text/html')
-  res.end('please read the documentation for this api')
+  res.end('please read the documentation for this api at http://chab.us')
+}
+
+function getStops(req, res) {
+  res.setHeader('content-type','application/json')
+  res.end(JSON.stringify(stops))
 }
 
 module.exports = server
